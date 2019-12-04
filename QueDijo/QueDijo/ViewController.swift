cb8c54b925d0f84c87423e206214a8848cf778ea
Andrea Amezcua Moreno <andrea@Andreas-MacBook-Pro-2.local>
2019-12-03 16:24:23 -0800

    initial commit

---
+++ QueDijo/QueDijo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QueDijo
+//
+//  Created by Andrea Amezcua Moreno on 12/3/19.
+//  Copyright © 2019 Andrea Amezcua Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
